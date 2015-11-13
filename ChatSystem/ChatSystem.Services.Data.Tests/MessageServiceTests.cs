5c8420f07f704f840bfc30732c8a475e40f627b3
h.slavov <slavov.hristo@gmail.com>
2015-11-13 12:48:37 +0200

    Added InMemoryRepository for unit tests of ChatMessages. Added five tests for now.

---
+++ ChatSystem/ChatSystem.Services.Data.Tests/MessageServiceTests.cs
@@ -0,0 +1,76 @@
+﻿namespace ChatSystem.Services.Data.Tests
+{
+    using TestObjects;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Linq;
+    using Services.Data.Contracts;
+    using Models;
+
+    [TestClass]
+    public class MessageServiceTests
+    {
+        private IMessagesService messageService;
+
+        private InMemoryRepository<ChatMessage> messageRepository;
+        private InMemoryRepository<User> userRepository;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            this.messageRepository = TestObjectFactory.GetMessageRepository();
+            this.userRepository = TestObjectFactory.GetUsersRepository();
+            this.messageService = new MessagesService(messageRepository,userRepository);
+        }
+
+        [TestMethod]
+        public void AddShouldReturnCorrentNumberOfSavedChanges()
+        {
+            this.messageService.Add("Message", "User1", "User5");
+            var actual = this.messageRepository.NumberOfSavedChanges;
+            Assert.AreEqual(1, actual);
+        }
+
+        [TestMethod]
+        public void AddShouldInsertCorrectValuesInRepository()
+        {
+            this.messageService.Add("Another message", "Username1", "Username8");
+            var actual = this.messageRepository.All().FirstOrDefault(ms => ms.Message.Contains("Another"));
+
+            Assert.IsNotNull(actual);
+            Assert.AreEqual("Username1", actual.Sender.UserName);
+            Assert.AreEqual("Another message", actual.Message);
+            Assert.AreEqual("Username8", actual.Receiver.UserName);
+        }
+
+        [TestMethod]
+        public void AllShouldReturnCorrectCountOfMessages()
+        {
+            var currentCount = this.messageRepository.All().Count();
+            Assert.AreEqual(20, currentCount);
+
+            this.messageService.Add("Message", "Username3", "Username5");
+            this.messageService.Add("Message", "Username5", "Username6");
+
+            var actual = this.messageRepository.All().Count();
+            Assert.AreEqual(22, actual);
+        }
+
+        [TestMethod]
+        public void RepositoryDisposeShouldReturnTrueWhenDisposed()
+        {
+            this.messageRepository.Dispose();
+            var actual = this.messageRepository.isDisposed;
+            Assert.AreEqual(true, actual);
+        }
+
+        [TestMethod]
+        public void RepositoryGetShouldReturnNotNullableValue()
+        {
+            //id is fake! should return the first value in the dummy base.
+            var actual = this.messageRepository.GetById("1");
+            Assert.IsNotNull(actual);
+            Assert.AreEqual("User0", actual.Sender.UserName);
+            Assert.AreEqual("User01", actual.Receiver.UserName);
+        }
+    }
+}
---
+++ ChatSystem/ChatSystem.Services.Data.Tests/TestObjects/TestObjectFactory.cs
@@ -0,0 +1,44 @@
+﻿namespace ChatSystem.Services.Data.Tests.TestObjects
+{
+    using System;
+    using ChatSystem.Models;
+
+    public static class TestObjectFactory
+    {
+        public static InMemoryRepository<ChatMessage> GetMessageRepository(int messages = 20)
+        {
+            var repository = new InMemoryRepository<ChatMessage>();
+
+            for (int i = 0; i < messages; i++)
+            {
+                var message = new ChatMessage();
+                message.Message = "Test message" + i;
+                message.Sender = new User() {  UserName = "User" + i, LastName = "User lastName" + i };
+                message.Receiver = new User() { UserName = "User" + i + 1, LastName = "User lastName" + i + 1};
+                message.SentOn = DateTime.Now;
+                repository.Add(message);
+            }
+
+            return repository;  
+        }
+
+        public static InMemoryRepository<User> GetUsersRepository(int usersCount = 20)
+        {
+            var repository = new InMemoryRepository<User>();
+
+            for (int i = 0; i < usersCount; i++)
+            {
+                var user = new User()
+                {
+                    UserName = "Username" + i,
+                    FirstName = "User" + i,
+                    LastName = "User lastName"  + i
+                };
+
+                repository.Add(user);
+            }
+
+            return repository;
+        }
+    }
+}
