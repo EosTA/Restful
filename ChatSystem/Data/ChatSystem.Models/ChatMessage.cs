c6ceb1feaf6dad912cdfeea5963ed8f525718792
Pavel <pawel.angelow@gmail.com>
2015-11-18 13:04:52 +0200

    Updated version

--- ChatSystem/Data/ChatSystem.Models/ChatMessage.cs
+++ ChatSystem/Data/ChatSystem.Models/ChatMessage.cs
@@ -3,10 +3,12 @@
     using System;
     using System.ComponentModel.DataAnnotations;
     using ChatSystem.Common.Constants;
+    using System.ComponentModel.DataAnnotations.Schema;
 
     public class ChatMessage
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
         public DateTime SentOn { get; set; }
